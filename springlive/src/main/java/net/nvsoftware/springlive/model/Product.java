f99442c7276c8ddfc4edea398a6f5aeba2546180
hehe-190 <18810660758@163.com>
2023-10-09 03:36:53 -0400

    create generic exceptionhandler and Security hidden some fields

--- springlive/src/main/java/net/nvsoftware/springlive/model/Product.java
+++ springlive/src/main/java/net/nvsoftware/springlive/model/Product.java
@@ -1,6 +1,11 @@
 package net.nvsoftware.springlive.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"title","description"})
 public class Product {
+    //@JsonIgnore
     private String productId;
     private String title;
     private String description;
