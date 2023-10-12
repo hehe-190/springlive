afba36aa9fb6b862b98a8867d88f22fda111a203
hehe-190 <18810660758@163.com>
2023-10-12 02:46:36 -0400

    For PaymentService, add PaymentResponse and getPaymentDetailByOrderId

--- PaymentService/src/main/java/net/nvsoftware/PaymentService/repository/PaymentRepository.java
+++ PaymentService/src/main/java/net/nvsoftware/PaymentService/repository/PaymentRepository.java
@@ -4,5 +4,6 @@ import net.nvsoftware.PaymentService.entity.PaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+    PaymentEntity findByOrderId(long orderId);
 
 }
