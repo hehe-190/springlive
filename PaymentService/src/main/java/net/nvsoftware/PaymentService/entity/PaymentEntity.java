ab556c0b35a92628d22c28eb3b6c2438f8902359
hehe-190 <18810660758@163.com>
2023-10-11 21:52:58 -0400

    config ProductService in yaml, and controller entity model repository service

---
+++ PaymentService/src/main/java/net/nvsoftware/PaymentService/entity/PaymentEntity.java
@@ -0,0 +1,27 @@
+package net.nvsoftware.PaymentService.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name="Payment_TB")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private long orderId;
+    private long paymentMode;
+    private long totalAmount;
+    private Instant paymentDate;
+    private String paymentStatus;
+
+}
