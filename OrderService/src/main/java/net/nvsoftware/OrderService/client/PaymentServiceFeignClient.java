49aa6c661fa876ae319ce46c1fc550874e904561
hehe-190 <18810660758@163.com>
2023-10-12 23:14:04 -0400

    add CircuitBreaker to OrderSrvice

--- OrderService/src/main/java/net/nvsoftware/OrderService/client/PaymentServiceFeignClient.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/client/PaymentServiceFeignClient.java
@@ -1,6 +1,8 @@
 package net.nvsoftware.OrderService.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import net.nvsoftware.OrderService.model.PaymentRequest;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name="PAYMENT-SERVICE/payment")
+@CircuitBreaker(name = "feignCall", fallbackMethod = "feignCallPaymentFallback")
 public interface PaymentServiceFeignClient {
     @PostMapping("/doPayment")
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void feighCallPaymentFallback(Exception e) {
+        throw new RuntimeException("PAYMENT-SERVICE is done");
+    }
 }
