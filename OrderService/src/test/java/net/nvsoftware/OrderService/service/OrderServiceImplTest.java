22c0a5223a96bd9108bd27835e2886653685d9ab
hehe-190 <18810660758@163.com>
2023-10-23 23:36:32 -0400

    add Mock Data, and Mock Part using Mockito to mimic method call's response

--- OrderService/src/test/java/net/nvsoftware/OrderService/service/OrderServiceImplTest.java
+++ OrderService/src/test/java/net/nvsoftware/OrderService/service/OrderServiceImplTest.java
@@ -2,16 +2,22 @@ package net.nvsoftware.OrderService.service;
 
 import net.nvsoftware.OrderService.client.PaymentServiceFeignClient;
 import net.nvsoftware.OrderService.client.ProductServiceFeignClient;
+import net.nvsoftware.OrderService.entity.OrderEntity;
+import net.nvsoftware.OrderService.model.OrderResponse;
 import net.nvsoftware.OrderService.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Inject;
+
+import java.time.Instant;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,12 +37,53 @@ class OrderServiceImplTest {
     @Test
     void testWhenGetOrderDetailByIdSuccess() {
         // Mock
-        
+        OrderEntity orderEntity = getMockOrderEntity();
+        Mockito.when(orderRepository.findById(Mockito.anyLong()))
+                .thenReturn(Optional.of(orderEntity));
+        Mockito.when(restTemplate.getForObject(
+                "http://PRODUCT-SERVICE/product/" + orderEntity.getProductId(),
+                OrderResponse.ProductResponse.class
+        )).thenReturn(getMockProductResponse());
+        Mockito.when(restTemplate.getForObject(
+                "http://PAYMENT-SERVICE/payment/" + orderEntity.getId(),
+                OrderResponse.PaymentResponse.class
+        )).thenReturn(getMockPaymentResponse());
         // Actual Call
         orderService.getOrderDetailById(1);
 
         // Verify Call
 
         // Assert Result
+    }
+
+    private OrderResponse.PaymentResponse getMockPaymentResponse() {
+        return OrderResponse.PaymentResponse.builder()
+                .orderId(7)
+                .id(1)
+                .paymentDate(Instant.now())
+                .paymentMode("VISA")
+                .paymentStatus("SUCCESS")
+                .totalAmount(2999)
+                .build();
+    }
+
+    private OrderResponse.ProductResponse getMockProductResponse() {
+        return OrderResponse.ProductResponse.builder()
+                .id(2)
+                .name("MacMini")
+                .quantity(2)
+                .price(1499)
+                .build();
+    }
+
+    private OrderEntity getMockOrderEntity() {
+        return OrderEntity.builder()
+                .id(7)
+                .productId(2)
+                .quantity(2)
+                .totalAmount(2999)
+                .orderDate(Instant.now())
+                .orderStatus("PLACED")
+                .build();
     }
 }
