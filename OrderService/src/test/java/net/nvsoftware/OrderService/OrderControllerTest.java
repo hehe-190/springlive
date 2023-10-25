ad443d1eb963fea37569b81216364dabfba98b9b
hehe-190 <18810660758@163.com>
2023-10-24 20:42:24 -0400

    service wiremock in port 8053 and link 8053 in serviceSupplierList

--- OrderService/src/test/java/net/nvsoftware/OrderService/OrderControllerTest.java
+++ OrderService/src/test/java/net/nvsoftware/OrderService/OrderControllerTest.java
@@ -1,18 +1,44 @@
 package net.nvsoftware.OrderService;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import net.nvsoftware.OrderService.repository.OrderRepository;
+import net.nvsoftware.OrderService.service.OrderService;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @EnableConfigurationProperties
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = OrderServiceTestConfig.class)
 public class OrderControllerTest {
-//    @Bean
-//    public  ServiceInstanceListSupplierTest supplierTest() {
-//        return new ServiceInstanceListSupplierTest();
-//    }
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @RegisterExtension
+    static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
+            .options(WireMockConfiguration.wireMockConfig().port(8053))
+            .build();
+
+    private ObjectMapper objectMapper = new ObjectMapper()
+            .findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 }
